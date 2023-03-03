25a989d66433a1b1c9050c9c82086106de5deaf8
Rohan8017 <114735723+Rohan8017@users.noreply.github.com>
2023-03-03 11:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+const rootElement = document.getElementById('root');
+const changeUserButton = document.getElementById('getUser');
+window.onload=()=>getUser();
+function getUser(){
+  fetch("https://randomuser.me/api/")
+    .then((data) => data.json())
+    .then((result) => {
+      const profile = result.results[0];
+      rootElement.innerHTML = `<div>
+      <img src=${profile.picture.large} alt='profile picture'/>
+      <h2>${profile.name.first} ${profile.name.last}</h2>
+      <div class="showhide age">${profile.dob.age}</div>
+      <div class="showhide email">${profile.email}</div>
+      <div class="showhide phone">${profile.phone}</div>
+      <div>`
+      const showHide = document.querySelectorAll('.showhide');
+      const showDiv = document.querySelectorAll('.show');
+      showDiv.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          triggeredButton=e.target.innerText.toLowerCase();
+          showHide.forEach((button)=>{
+            if(button.classList.contains(`${triggeredButton}`)){
+              button.classList.remove('showhide');
+            }else{
+              button.classList.add('showhide');
+            }
+          })
+  
+        })
+      })
+  
+    })
+}
+changeUserButton.addEventListener('click',getUser);
